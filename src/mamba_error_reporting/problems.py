8a4ec1fb767f53f2a197febdf6ff94507d1312c6
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-08-02 18:31:53 +0200

    Add problem with missing dependencies

--- src/mamba_error_reporting/problems.py
+++ src/mamba_error_reporting/problems.py
@@ -93,43 +93,56 @@ def create_pubgrub() -> tuple[libmambapy.Solver, libmambapy.Pool]:
     )
 
 
-def create_pubgrub_hard() -> tuple[libmambapy.Solver, libmambapy.Pool]:
+def create_pubgrub_hard(missing_package: bool = False) -> tuple[libmambapy.Solver, libmambapy.Pool]:
+    packages = [
+        {"name": "menu", "version": "2.1.0", "dependencies": ["dropdown>=2.1", "emoji"]},
+        {"name": "menu", "version": "2.0.1", "dependencies": ["dropdown>=2", "emoji"]},
+        {"name": "menu", "version": "2.0.0", "dependencies": ["dropdown>=2", "emoji"]},
+        {"name": "menu", "version": "1.5.0", "dependencies": ["dropdown=2.*", "emoji"]},
+        {"name": "menu", "version": "1.4.0", "dependencies": ["dropdown=2.*", "emoji"]},
+        {"name": "menu", "version": "1.3.0", "dependencies": ["dropdown=2.*"]},
+        {"name": "menu", "version": "1.2.0", "dependencies": ["dropdown=2.*"]},
+        {"name": "menu", "version": "1.1.0", "dependencies": ["dropdown=1.*"]},
+        {"name": "menu", "version": "1.0.0", "dependencies": ["dropdown=1.*"]},
+        {"name": "emoji", "version": "1.1.0", "dependencies": ["libicons=2.*"]},
+        {"name": "emoji", "version": "1.0.0", "dependencies": ["libicons=2.*"]},
+        {"name": "dropdown", "version": "2.3.0", "dependencies": ["libicons=2.*"]},
+        {"name": "dropdown", "version": "2.2.0", "dependencies": ["libicons=2.*"]},
+        {"name": "dropdown", "version": "2.1.0", "dependencies": ["libicons=2.*"]},
+        {"name": "dropdown", "version": "2.0.0", "dependencies": ["libicons=2.*"]},
+        {"name": "dropdown", "version": "1.8.0", "dependencies": ["libicons=1.*", "intl=3.*"]},
+        {"name": "dropdown", "version": "1.7.0", "dependencies": ["libicons=1.*", "intl=3.*"]},
+        {"name": "dropdown", "version": "1.6.0", "dependencies": ["libicons=1.*", "intl=3.*"]},
+        {"name": "pyicons", "version": "2.0.0", "dependencies": ["libicons=2.*"]},
+        {"name": "pyicons", "version": "1.0.0", "dependencies": ["libicons=1.*"]},
+        {"name": "intl", "version": "5.0.0"},
+        {"name": "intl", "version": "4.0.0"},
+        {"name": "intl", "version": "3.2.0"},
+        {"name": "intl", "version": "3.1.0"},
+        {"name": "intl", "version": "3.0.0"},
+        {"name": "libicons", "version": "2.1.0"},
+        {"name": "libicons", "version": "2.0.1"},
+        {"name": "libicons", "version": "2.0.0"},
+        {"name": "libicons", "version": "1.2.1"},
+        {"name": "libicons", "version": "1.2.0"},
+        {"name": "libicons", "version": "1.0.0"},
+    ]
+    if missing_package:
+        packages += [
+            {"name": "dropdown", "version": "2.9.3", "dependencies": ["libnothere>1.0"]},
+            {"name": "dropdown", "version": "2.9.2", "dependencies": ["libicons>10.0", "libnothere>1.0"]},
+            {"name": "dropdown", "version": "2.9.1", "dependencies": ["libicons>10.0", "libnothere>1.0"]},
+            {"name": "dropdown", "version": "2.9.0", "dependencies": ["libicons>10.0"]},
+        ]
+
     return _create_problem_manual(
-        packages=[
-            {"name": "menu", "version": "2.1.0", "dependencies": ["dropdown>=2.1", "emoji"]},
-            {"name": "menu", "version": "2.0.1", "dependencies": ["dropdown>=2", "emoji"]},
-            {"name": "menu", "version": "2.0.0", "dependencies": ["dropdown>=2", "emoji"]},
-            {"name": "menu", "version": "1.5.0", "dependencies": ["dropdown=2.*", "emoji"]},
-            {"name": "menu", "version": "1.4.0", "dependencies": ["dropdown=2.*", "emoji"]},
-            {"name": "menu", "version": "1.3.0", "dependencies": ["dropdown=2.*"]},
-            {"name": "menu", "version": "1.2.0", "dependencies": ["dropdown=2.*"]},
-            {"name": "menu", "version": "1.1.0", "dependencies": ["dropdown=1.*"]},
-            {"name": "menu", "version": "1.0.0", "dependencies": ["dropdown=1.*"]},
-            {"name": "emoji", "version": "1.1.0", "dependencies": ["libicons=2.*"]},
-            {"name": "emoji", "version": "1.0.0", "dependencies": ["libicons=2.*"]},
-            {"name": "dropdown", "version": "2.3.0", "dependencies": ["libicons=2.*"]},
-            {"name": "dropdown", "version": "2.2.0", "dependencies": ["libicons=2.*"]},
-            {"name": "dropdown", "version": "2.1.0", "dependencies": ["libicons=2.*"]},
-            {"name": "dropdown", "version": "2.0.0", "dependencies": ["libicons=2.*"]},
-            {"name": "dropdown", "version": "1.8.0", "dependencies": ["libicons=1.*", "intl=3.*"]},
-            {"name": "dropdown", "version": "1.7.0", "dependencies": ["libicons=1.*", "intl=3.*"]},
-            {"name": "dropdown", "version": "1.6.0", "dependencies": ["libicons=1.*", "intl=3.*"]},
-            {"name": "pyicons", "version": "2.0.0", "dependencies": ["libicons=2.*"]},
-            {"name": "pyicons", "version": "1.0.0", "dependencies": ["libicons=1.*"]},
-            {"name": "libicons", "version": "2.1.0"},
-            {"name": "libicons", "version": "2.0.1"},
-            {"name": "libicons", "version": "2.0.0"},
-            {"name": "libicons", "version": "1.2.1"},
-            {"name": "libicons", "version": "1.2.0"},
-            {"name": "libicons", "version": "1.0.0"},
-            {"name": "intl", "version": "5.0.0"},
-            {"name": "intl", "version": "4.0.0"},
-            {"name": "intl", "version": "3.2.0"},
-            {"name": "intl", "version": "3.1.0"},
-            {"name": "intl", "version": "3.0.0"},
-        ],
+        packages=packages,
         specs=["menu", "pyicons=1.*", "intl=5.*"],
     )
+
+
+def create_pubgrub_missing() -> tuple[libmambapy.Solver, libmambapy.Pool]:
+    return create_pubgrub_hard(True)
 
 
 def create_conda_forge(specs: Sequence[str]) -> tuple[libmambapy.Solver, libmambapy.Pool]:
@@ -149,6 +162,7 @@ def create_pytorch() -> tuple[libmambapy.Solver, libmambapy.Pool]:
 
 def create_r_base() -> tuple[libmambapy.Solver, libmambapy.Pool]:
     return create_conda_forge(["r-base=3.5.* ", "pandas=0", "numpy<1.20.0", "matplotlib=2", "r-matchit=4.*"])
+
 
 def create_scip() -> tuple[libmambapy.Solver, libmambapy.Pool]:
     return create_conda_forge(["scip=8.*", "pyscipopt<4.0"])
