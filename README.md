f2737bf2a64ffae382465f588724605a06ab7192
AntoinePrv <AntoinePrv@users.noreply.github.com>
2022-08-10 15:15:02 +0200

    Update install instructions

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Proof of concept for improving mamba errors message about unsolvable environment
 ## Installation
 ```bash
 micromamba create -n mamba-error --file environment.yml
-micromamba activate mamba-dev
+micromamba activate mamba-error
 python -m pip install -e .
+# Currently requires unreleased features of libmambapy
+./install-mamba-latest.sh
 ```
--- install-mamba-latest.sh
+++ install-mamba-latest.sh
@@ -1,10 +1,15 @@
 #! /usr/bin/env bash
 
-cd "$(mktemp -d)"
-curl -sLJ 'https://github.com/mamba-org/mamba/archive/master.tar.gz' | tar -xz -
-micromamba install --file mamba-master/mamba/environment-dev.yml
-cmake -B build/ -S mamba-master/ \
-	-D BUILD_SHARED=ON -D BUILD_LIBMAMBA=ON -D BUILD_LIBMAMBAPY=ON
+readonly tmp_dir="$(mktemp -d)"
+trap 'rm -rf "${tmp_dir}" || true' EXIT
+
+cd "${tmp_dir}"
+micromamba activate mamba-error
+curl -sLJ 'https://github.com/mamba-org/mamba/archive/master.tar.gz' | tar -xz
+
+cd mamba-master
+micromamba install -n mamba-error --file mamba/environment-dev.yml
+cmake -B build/ -D BUILD_SHARED=ON -D BUILD_LIBMAMBA=ON -D BUILD_LIBMAMBAPY=ON
 cmake --build build/ --parallel
 cmake --install build/ --prefix "${CONDA_PREFIX}"
-python -m pip install mamba-master/libmambapy/ mamba-master/mamba/
+python -m pip install --ignore-installed --no-deps --no-build-isolation libmambapy/ mamba/
